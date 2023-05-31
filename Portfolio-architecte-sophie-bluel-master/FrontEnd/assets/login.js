06b0e79eae12119bb8bdbd615121c4edc6de8091
mathis13640 <mathis13640@gmail.com>
2023-05-31 17:36:03 +0200

    login done

---
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/login.js
@@ -0,0 +1,40 @@
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  
+  const user = {
+    email: email,
+    password: password
+  };
+
+  //Envoie données à l'API
+  fetch("http://localhost:5678/api/users/login", {
+    method: 'POST',
+    body: JSON.stringify(user),
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    }
+    
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json(); // Converted response in JSON
+    } else {
+      throw new Error('La combinaison utilisateur - mot de passe est incorrecte.');
+    }
+  })
+  .then(data => {
+    const token = data.token; // retrieve the token from the JSON reponse
+    localStorage.setItem('authToken', token); // Stock the token in the localStorage
+    
+    window.location.href = './index.html'; // Redirige the user to the home page
+  })
+  .catch(error => { //Display an ERROR message i
+    alert(error.message);
+    console.error('Une erreur s\'est produite:', error);
+  });
+});
